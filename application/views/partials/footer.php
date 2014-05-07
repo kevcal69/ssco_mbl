73af102a32332c9d7db4b89e29d65f2368635ccb
paultitus.obligado <paultitus.obligado@gmail.com>
2014-05-07 18:11:56 +0800

    Changed footer; updated template.css

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<div id="ncr">
-		<a href="http://www.ncr.com/"><img src="<?= base_url() . 'assets/images/landing/ncr-logo-small.jpg';?>" height="45"></a>
+		<a href="http://www.ncr.com/"><img src="<?= base_url() . 'assets/images/landing/NCR-White-transp.png';?>"></a>
 		<span id="copyright">© 2014 NCR CORPORATION. ALL RIGHTS RESERVED.</span>
 	</div>
 </div>
