eb210d0d3f84bbf424d04cd7b11ab4097b76384e
Kev Cal <kevcal69@gmail.com>
2014-05-09 05:46:11 -0400

    commit changes for new branch sidebar

--- application/views/admin/question/create.php
+++ application/views/admin/question/create.php
@@ -1,4 +1,38 @@
 <div id="options">
-	<h1>Question and Evaluation Settings</h1>
-	<i class = "fa fa-camera-retro fa-lg"></i><h2>Options</h2>
+	<div id="side-bar">
+		<h3>Quick Links</h3>
+			<ul>
+				<li><i class = "fa fa-plus-circle fa-small2x"></i><span>Users</span></li>
+				<li><i class = "fa fa-bars fa-small2x"></i><span>Modules</span></li>
+			</ul>		
+		<h3>Options</h3>
+		<div id="module-profile">
+			<div id = "thumb" style = "background-image: url(<?=base_url() . $module->cover_picture;?>);"></div>
+			<div id="module-title">
+				<i class = "fa fa-bookmark fa-small icon-box"></i><?=$module->title;?>
+			</div>
+		</div>
+		<div id="module-stat">
+			<ul>
+				<li><span>Number of Questions: </span><span>10</span></li>
+				<li><span>Link to the module:</span><span>here</span></li>
+			</ul>
+			
+			
+		</div>
+		<h3>Actions</h3>
+		<div id="actions">
+			<ul>
+				<li><i class = "fa fa-plus-circle fa-small2x"></i><span>Add Questions</span></li>
+				<li><i class = "fa fa-bars fa-small2x"></i><span>List all Questions</span></li>
+				<li><i class = "fa fa-desktop fa-small2x"></i><span>Set EvalutaionTest</span></li>
+				<li><i class = "fa fa-tags fa-small2x"></i><span>Schedule a Test</span></li>
+				<li><i class = "fa fa-bar-chart-o fa-small2x"></i><span>Users Stats</span></li>
+			</ul>
+		</div>
+	</div>
+	<div id="opt-content">
+		
+	</div>
+
 </div>
